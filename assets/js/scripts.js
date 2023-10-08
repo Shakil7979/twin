177c8effc1618cb0f98028abb784f5ecffe731ff
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2023-10-09 01:59:06 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,4 +6,41 @@ $(document).ready(function(){
         items:3,
         margin: 40
     });
+
+
+    // script for popup 
+
+    $('.weldone').show();
+    $('.popup_overlay').show();
+
+    $(document).on('click','.yeah_sure',function(){ 
+        $('.dashboard_popup').show();
+        $('.popup_overlay').show();
+        $('.weldone').hide();
+    });
+
+    $(document).on('click','.close_popup a',function(){ 
+        $('.dashboard_popup').hide();
+        $('.popup_overlay').hide(); 
+    });
+
+    $(document).on('click','.btn_not_now',function(){  
+        $('.weldone').hide();
+        $('.popup_overlay').hide();
+    });
+
+
+    // chat box script  
+    $(document).on('click','.banner_chat',function(){  
+        $('.chat_area').show(); 
+    });
+    $(document).on('click','.chat_close',function(){  
+        $('.chat_area').hide(); 
+    });
+
+
+
+
+
+
 })
